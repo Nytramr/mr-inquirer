4334f476f8925812bd4074752fa5b3ead8a09713
Martin Rubinsztein <m.rubinsztein@globant.com>
2015-04-29 18:19:13 -0300

    Adding path prompt files

---
+++ examples/path.js
@@ -0,0 +1,51 @@
+/**
+ * Checkbox list examples
+ */
+
+"use strict";
+var inquirer = require("inquirer");
+
+inquirer.prompt.registerPrompt('tree', require('../lib/prompts/path.js'));
+
+inquirer.prompt([
+  {
+    type: "checkbox",
+    message: "Select toppings",
+    name: "toppings",
+    choices: [
+      new inquirer.Separator("The usual:"),
+      {
+        name: "Peperonni"
+      },
+      {
+        name: "Cheese",
+        checked: true
+      },
+      {
+        name: "Mushroom"
+      },
+      new inquirer.Separator("The extras:"),
+      {
+        name: "Pineapple",
+      },
+      {
+        name: "Bacon"
+      },
+      {
+        name: "Olives",
+        disabled: "out of stock"
+      },
+      {
+        name: "Extra cheese"
+      }
+    ],
+    validate: function( answer ) {
+      if ( answer.length < 1 ) {
+        return "You must choose at least one topping.";
+      }
+      return true;
+    }
+  }
+], function( answers ) {
+  console.log( JSON.stringify(answers, null, "  ") );
+});
