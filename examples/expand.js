c923ca5587119b8e34f0e43e6cacc4f2ea557058
Nytramr <martin@brooklynpacket.com>
2015-06-05 01:15:57 -0300

    Showing current directory content

--- examples/expand.js
+++ examples/expand.js
@@ -9,31 +9,9 @@ inquirer.prompt.registerPrompt('path', require('../lib/prompts/expand.js'));
 inquirer.prompt([
   {
     type: "path",
-    message: "Conflict on `file.js`: ",
+    message: "Enter path: ",
     name: "overwrite",
-    choices: [
-      {
-        key: "y",
-        name: "Overwrite",
-        value: "overwrite"
-      },
-      {
-        key: "a",
-        name: "Overwrite this one and all next",
-        value: "overwrite_all"
-      },
-      {
-        key: "d",
-        name: "Show diff",
-        value: "diff"
-      },
-      new inquirer.Separator(),
-      {
-        key: "x",
-        name: "Abort",
-        value: "abort"
-      }
-    ]
+    cwd: '/'
   }
 ], function( answers ) {
   console.log( JSON.stringify(answers, null, "  ") );
