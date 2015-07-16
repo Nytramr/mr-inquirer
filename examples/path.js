b4989976ffa375bad0b2d68e3da70a84313e2e60
Nytramr <martin@brooklynpacket.com>
2015-07-16 00:17:17 -0300

    Bash mode is working

--- examples/path.js
+++ examples/path.js
@@ -1,24 +1,17 @@
 /**
- * Checkbox list examples
+ * Expand list examples
  */
 
 "use strict";
 var inquirer = require("inquirer");
-
 inquirer.prompt.registerPrompt('path', require('../lib/prompts/path.js'));
 
 inquirer.prompt([
   {
     type: "path",
-    message: "Select toppings",
-    name: "toppings",
-    cwd: '/Users/user/',
-    validate: function( answer ) {
-      if ( answer.length < 1 ) {
-        return "You must choose at least one topping.";
-      }
-      return true;
-    }
+    message: "Enter path: ",
+    name: "overwrite",
+    cwd: '/'
   }
 ], function( answers ) {
   console.log( JSON.stringify(answers, null, "  ") );
